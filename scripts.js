8e39f74f2ea6f2802e86a160d696b5cdec53bcbf
Sullivown <daniel.w.t.sullivan@gmail.com>
2021-10-04 17:39:49 +0100

    Added keyobard control and added visutal styling

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 // Initialize variables
-const canvas = document.querySelector('#canvas');
-const resetButton = document.querySelector('#reset-button');
-const brushSelect = document.querySelector('#brush-select');
 let gridSize = 16;
-/* let brush = 'grey';
-let fadeBrushLightness = 90;
-let fadeBrushDecrease = true;
-*/
+let numberOfCells = gridSize * gridSize;
+
 let brush = {
     type: 'grey',
     location: 0,
@@ -19,22 +14,30 @@ let brush = {
 }
 
 // Add initial event listeners
+const canvas = document.querySelector('#canvas');
+const resetButton = document.querySelector('#reset-button');
+const brushSelect = document.querySelector('#brush-select');
+
 document.addEventListener('DOMContentLoaded', createGrid);
 resetButton.addEventListener('click', reset);
 brushSelect.addEventListener('change', changeBrush);
+window.addEventListener('keyup', moveBrush)
+
 
 //Function to add empty grid to the page
 function createGrid() {
-    // Calculate the total number of cells
-    numberOfCells = gridSize * gridSize;
+
+    // Reset brush location
+    brush.location = 0;
 
     // Set CSS grid parameters for canvas to create the grid
     canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
     canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
 
     // Create as many cells (divs) as required and add them to the canvas
-    for (let i = 0; i < numberOfCells; i++) {
+    for (let i = 1; i <= numberOfCells; i++) {
         let cell = document.createElement('div');
+        cell.setAttribute('id', `cell-${i}`)
         cell.classList.add('single-cell')
         canvas.appendChild(cell);
     }
@@ -45,28 +48,48 @@ function createGrid() {
     })
 }   
 
-// Function to fill the cell with colour on mouse over
-function fillCell() {
-    if (brush.type === 'grey'){
-        this.style.backgroundColor = 'hsl(0, 0%, 50%)';
-    } else if (brush.type === 'random') {
-        this.style.backgroundColor = randomBrush();
-    } else if (brush.type === 'fade') {
-        this.style.backgroundColor = fadeBrush();
+// Function to fill the cell with colour
+function fillCell(event) {
+    let cellId = 0;
+
+    // Check for event
+    if (event.type === 'mouseover') {
+        cellId = this.id;
+    } else if (event.type === 'keyup') {
+        cellId = `cell-${brush.location}`;
     }
 
+    const cell = document.getElementById(cellId);
+
+    if (brush.type === 'grey'){
+        cell.style.backgroundColor = 'hsl(0, 0%, 50%)';
+    } else if (brush.type === 'random') {
+        cell.style.backgroundColor = randomBrush();
+    } else if (brush.type === 'fade') {
+        cell.style.backgroundColor = fadeBrush();
+    }
+
+
     // Set the fade in animation
-    this.classList.add('fade-in-cell');
+    cell.classList.add('fade-in-cell');
+
+    // Set the brush location to this square
+    brush.location = parseInt(cellId.split('-').pop());
 }
 
 // Function to reset grid and adjust size
 function reset() {
 
+    let promptInput = 16;
+
     // Only allow numerical values > 0 and <= 100 as a valid size
     do {
-    gridSize = prompt('Grid size:', 16)
+        promptInput = prompt('Grid size:', 16)
     } while (isNaN(gridSize) || gridSize < 1 || gridSize > 100);
     
+    gridSize = parseInt(promptInput);
+    numberOfCells = gridSize * gridSize;
+
     canvas.innerHTML = '';
     createGrid();
 }
@@ -101,4 +124,42 @@ function fadeBrush() {
     }
 
     return `hsl(0, 0%, ${currentLightness}%)`;
+}
+
+// Checks if a cell is exists
+function validCell(cellNumber) {
+    if (cellNumber < 1 || cellNumber > numberOfCells) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function moveBrush(event) {
+    console.log("BRUSH location: " + brush.location);
+    let newLocation = 0;
+
+    // Calculate the new brush location
+    if (event.key === 'ArrowLeft') {
+        console.log(typeof(gridSize))
+        newLocation = brush.location - 1;
+    } else if (event.key === 'ArrowRight') {
+        console.log(typeof(gridSize))
+        newLocation = brush.location + 1;
+    } else if (event.key === 'ArrowUp') {
+        console.log(typeof(gridSize))
+        newLocation = brush.location - gridSize;
+    } else if (event.key === 'ArrowDown') {
+        console.log(typeof(gridSize))
+        newLocation = brush.location + gridSize;
+    }
+    console.log("NEW location: " + newLocation)
+    // If cell exists color it and move 
+    if (validCell(newLocation)) {
+        // Move the brush location
+        brush.location = newLocation;
+
+        // Color new cell
+        fillCell(event);
+    } 
 }
